29e5ce786edefcd848d55821f341923507bf5eee
Brandon Myers <bmyers@mozilla.com>
2018-12-05 16:00:05 -0500

    Add logic to retrieve well known

--- federated_boto/cli.py
+++ federated_boto/cli.py
@@ -2,6 +2,13 @@
 
 import click
 from config import parse_config
+import requests
+
+
+def retrieve_well_known(well_known_url):
+    resp = requests.get(well_known_url)
+    data = resp.json()
+    return data['token_endpoint'], data['authorization_endpoint']
 
 
 @click.command()
@@ -10,6 +17,9 @@ from config import parse_config
 def main(config_file, role_arn):
     # Parse config file
     config = parse_config(config_file)
+    config['token_endpoint'], config['authorization_endpoint'] = retrieve_well_known(config['well_known_url'])
+
+    print(config)
     click.echo("Obtaining temporary credentials for {0}".format(role_arn))
     return 0
 
