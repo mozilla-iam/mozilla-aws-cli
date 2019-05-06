eb3a8e4c49cb5ab4e4060cbafa32ae42012623e4
Gene Wood <gene_wood@cementhorizon.com>
2019-05-06 11:39:59 -0700

    Remove unneeded test invocation

--- tests/test_federated_boto.py
+++ tests/test_federated_boto.py
@@ -29,7 +29,6 @@ def test_content(response):
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main)
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert 'Show this message and exit.' in help_result.output
