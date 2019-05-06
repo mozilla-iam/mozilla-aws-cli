fb96068553146b1d9f94034944aea20802ab51b1
Guillaume Destuynder <kang+mozilla@insecure.ws>
2019-05-06 11:14:07 -0700

    missing logging functionality and hanlding of exception

--- federated_aws_cli/config.py
+++ federated_aws_cli/config.py
@@ -1,9 +1,13 @@
 import yaml
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def parse_config(file_location):
     try:
-        with open(file_location, 'r') as stream:
+        with open(file_location, "r") as stream:
             return yaml.load(stream)
-    except:
+    except Exception as e:
+        logger.error("Could not open configuration file {}: {}".format(file_location, e))
         return {}
