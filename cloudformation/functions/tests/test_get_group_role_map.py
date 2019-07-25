2505eaa597c62cbf40012a0ac61cecae8a2cc818
Gene Wood <gene_wood@cementhorizon.com>
2019-07-25 10:14:44 -0700

    Add type hints
    Rename colliding name arg variable
    Expand pagination function for readability
    Add example input and output to flip_map
    Reformat with black
    Remove redundant groups list creation and extension

--- cloudformation/functions/tests/test_get_group_role_map.py
+++ cloudformation/functions/tests/test_get_group_role_map.py
@@ -1,9 +1,11 @@
 import sys
+
 sys.path.append('..')
 
 import boto3
 from moto import mock_iam, mock_sts
 from get_group_role_map import get_group_role_map
+
 
 @mock_iam
 @mock_sts
@@ -33,12 +35,14 @@ def test_get_role_group_map():
     response = client.create_role(
         RoleName='TestRoleToAssume',
         AssumeRolePolicyDocument='{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":{"AWS":"arn:aws:iam::123456789012:root"},"Action":"sts:AssumeRole"}]}',
-        Description='Test role to assume')
+        Description='Test role to assume',
+    )
     role_to_assume_arn = response['Role']['Arn']
     response = client.create_role(
         RoleName='TestRoleWithFederatedConditions',
         AssumeRolePolicyDocument=test_assume_role_policy_document_with_federated_conditions,
-        Description='Test role with federated conditions')
+        Description='Test role with federated conditions',
+    )
     groups = get_group_role_map([role_to_assume_arn])
 
     assert len(groups) == 0
