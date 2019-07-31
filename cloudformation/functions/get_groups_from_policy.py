6cd5e095875a1b838f17a89d96d8404e31d1d282
April King <april@mozilla.com>
2019-07-31 14:06:02 -0500

    Add function to extract groups from AWS policies

---
+++ cloudformation/functions/get_groups_from_policy.py
@@ -0,0 +1,75 @@
+import logging
+import sys
+
+from json import loads
+from json.decoder import JSONDecodeError
+
+logger = logging.getLogger(__name__)
+
+UNGLOBBABLE_OPERATORS = ("StringEquals", "ForAnyValue:StringEquals")
+VALID_FEDERATED_PRINCIPAL_KEYS = (
+    "arn:aws:iam::656532927350:oidc-provider/auth-dev.mozilla.auth0.com/",
+    "arn:aws:iam::371522382791:oidc-provider/auth.mozilla.auth0.com/",
+)
+
+
+# Python 2 compatibility
+if sys.version_info > (3, 0):
+    basestring = str
+
+
+def __get_groups():
+    pass
+
+
+def get_groups_from_policy(policy) -> list:
+    # groups will be stored as a set to prevent duplicates and then return
+    # a list when everything is finished
+    policy_groups = set()
+
+    # be flexible on being passed a dictionary (parsed policy) or a string
+    # (unparsed policy)
+    if isinstance(policy, basestring):
+        try:
+            policy = loads(policy)
+        except JSONDecodeError:
+            return []
+
+    if not isinstance(policy, dict):
+        raise ValueError
+
+    # If policy lacks a statement, we can bail out
+    if 'Statement' not in policy:
+        return []
+
+    for statement in policy["Statement"]:
+        if (statement.get("Effect") != "Allow" or
+                statement.get("Action") != "sts:AssumeRoleWithWebIdentity" or
+                statement.get('Principal', {}).get('Federated') not in
+                VALID_FEDERATED_PRINCIPAL_KEYS):
+            continue
+
+        # For clarity:
+        # operator --> StringEquals, ForAnyValue:StringLike
+        # conditions --> dictionary mapping, e.g. StringEquals: {}
+        # condition: auth-dev.mozilla.auth0.com/:amr
+        for operator, conditions in statement.get("Condition", {}).items():
+            for condition in conditions:
+                if condition.endswith(":amr"):
+                    groups = conditions[condition]
+                    groups = [groups] if isinstance(groups, basestring) \
+                        else groups
+
+                    # Only the StringLike operator allows globbing or ?
+                    # Technically the * and ? values are legal in StringEquals,
+                    # but we don't allow them for clarity
+                    if (operator in UNGLOBBABLE_OPERATORS and
+                        any([
+                            ["*" in group for group in groups],
+                            [":" in group for group in groups]
+                            ])):
+                        raise ValueError
+
+                    policy_groups.update(groups)
+
+    return list(policy_groups)
---
+++ cloudformation/functions/tests/test_get_groups_from_policy.py
@@ -0,0 +1,42 @@
+import json
+import os
+import os.path
+import sys
+from cloudformation.functions.get_groups_from_policy import (
+    get_groups_from_policy
+)
+
+# P2 / P3 compatibility
+if sys.version_info > (3, 0):
+    import builtins
+else:
+    import __builtins__ as builtins
+
+
+# Open every policy in the policies directory
+__policies_dir = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "policies"
+)
+policies = []
+for filename in os.listdir(__policies_dir):
+    with open(os.path.join(__policies_dir, filename), "r") as f:
+        policies.append(json.load(f))
+
+
+def test_all_policies():
+    for policy in policies:
+        if "Returns" in policy:
+            assert(sorted(get_groups_from_policy(policy)) ==
+                   sorted(policy["Returns"]))
+        elif "Exception" in policy:
+            try:
+                get_groups_from_policy(policy)
+            except getattr(builtins, policy["Exception"]):
+                pass
+        else:
+            raise ValueError
+
+
+if __name__ == "__main__":
+    test_all_policies()
