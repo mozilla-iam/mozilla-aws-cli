b989b0d0584fedae14ed5785d8841ea1cc721f64
April King <april@mozilla.com>
2019-08-23 17:53:18 -0500

    Convert to Flask, rewrite huge chunks of the CLI

---
+++ federated_aws_cli/static/index.js
@@ -0,0 +1,20 @@
+// we fire on load, as that means all the images, fonts, etc. are loaded
+window.addEventListener("load", () => {
+   const url = new URL(document.location);
+
+   // make a fetch request
+   fetch("/redirect_callback", {
+       method: "POST",
+       body: JSON.stringify({
+           code: url.searchParams.get("code"),
+           state: url.searchParams.get("state"),
+       }),
+       headers: {
+           "Content-Type": "application/json",
+       },
+   }).then((response) => {
+       console.log("Successfully posted with", response);
+   }).catch((error) => {
+       console.error("Unable to POST to callback", error);
+   });
+});
