4b433604ab2c651346e605da527e1b95a2d43220
Gene Wood <gene_wood@cementhorizon.com>
2019-09-19 09:50:00 -0700

    Fix PEP008 line length violation

--- federated_aws_cli/login.py
+++ federated_aws_cli/login.py
@@ -194,7 +194,8 @@ class Login:
                 logger.debug(
                     'Roles and aliases are {}'.format(roles_and_aliases))
                 try:
-                    self.role_arn = show_role_picker(roles_and_aliases, message)
+                    self.role_arn = show_role_picker(
+                        roles_and_aliases, message)
                 except NoPermittedRoles as e:
                     logger.error(e)
                     break
