ea971a691971d328d1754c61b70167ab9cb2b50f
Gene Wood <gene_wood@cementhorizon.com>
2019-11-19 17:46:01 -0800

    Release v0.0.2
    Open up the dependent package versions to accept newer versions
    Add a changelog

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ with open("README.md") as readme_file:
 
 requirements = [
     "Click>=6.0",
-    "flask==1.0.2",
-    "requests==2.20.1",
+    "flask>=1.0.2",
+    "requests>=2.20.1",
     "console-menu",
     "python-jose",
 ]
@@ -55,6 +55,6 @@ setup(
     tests_require=test_requirements,
     extras_require=extras,
     url="https://github.com/mozilla-iam/mozilla-aws-cli",
-    version="0.0.1",
+    version="0.0.2",
     zip_safe=False,
 )
