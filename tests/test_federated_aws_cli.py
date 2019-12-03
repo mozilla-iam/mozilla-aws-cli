72f6b1307ff2f937e28128f628eef44319f87105
April King <april@mozilla.com>
2019-12-03 14:43:10 -0600

    Rewrite a good chunk of the tests to not require so many settings per test.

--- tests/test_federated_aws_cli.py
+++ tests/test_federated_aws_cli.py
@@ -3,7 +3,8 @@
 
 """Tests for `mozilla_aws_cli` package."""
 
-import os
+import uuid
+
 from click.testing import CliRunner
 from mozilla_aws_cli import cli
 
@@ -11,49 +12,67 @@ from mozilla_aws_cli import cli
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
-    help_result = runner.invoke(cli.main, ['--help'])
+    help_result = runner.invoke(cli.main, ["--help"])
     assert help_result.exit_code == 0
-    assert 'Show this message and exit.' in help_result.output
+    assert "Show this message and exit." in help_result.output
 
 
 def test_parse_config():
-    good_arn = 'arn:aws:iam::123456789012:role/MyRole'
-    bad_arn = 'bogus'
-    good_config_content = '''[DEFAULT]
+    good_config_content = """[DEFAULT]
 well_known_url = http://auth.example.com/.well-known/openid-configuration
 client_id = abcdefghijklmnopqrstuvwxyz012345
-idtoken_for_roles_url = https://example.com/roles'''
+idtoken_for_roles_url = https://example.com/roles"""
+
+    bad_arn = "bogus"
+    bad_output_setting = good_config_content + "\noutput = blahblahblah"
+    good_arn = "arn:aws:iam::123456789012:role/MyRole"
+    missing_config_setting = """[DEFAULT]
+well_known_url = http://auth.example.com/.well-known/openid-configuration
+    """
+
     cases = {
-        'malformed ini': {
-            'filename': os.path.expanduser(os.path.join("~", ".malformed")),
-            'content': '- a z=x:\ny: "${z}"',
-            'args': ['-c', os.path.expanduser(os.path.join("~", ".malformed")), '-r', good_arn]
+        "malformed ini": {
+            "content": "- a z=x:\ny: \"${z}\"",
+            "args": ["-r", good_arn]
         },
-        'missing config setting': {
-            'filename': os.path.expanduser(os.path.join("~", ".missing_config_setting")),
-            'content': '[DEFAULT]\nwell_known_url = http://auth.example.com/.well-known/openid-configuration\n',
-            'args': ['-c', os.path.expanduser(os.path.join("~", ".missing_config_setting")), '-r', good_arn]
+        "missing config setting": {
+            "content": missing_config_setting,
+            "args": ["-r", good_arn]
         },
-        'bad role arn': {
-            'filename': os.path.expanduser(os.path.join("~", ".good_config")),
-            'content': good_config_content,
-            'args': ['-c', os.path.expanduser(os.path.join("~", ".good_config")), '-r', bad_arn]
-        }
+        "bad role arn": {
+            "content": good_config_content,
+            "args": ["-r", bad_arn]
+        },
+        "bad output setting": {
+            "content": bad_output_setting,
+            "args": ["-r", good_arn]
+        },
     }
 
     runner = CliRunner()
     with runner.isolated_filesystem():
         results = {}
         for case in cases:
-            with open(cases[case]['filename'], 'w') as f:
-                f.write(cases[case]['content'])
-            results[case] = runner.invoke(cli.main, cases[case]['args'])
+            args = cases[case]["args"]
 
-        assert results['malformed ini'].exit_code != 0
-        assert 'is not a valid INI' in results['malformed ini'].output
+            # Use a random filename, if "-c" isn"t specified in the arguments above
+            filename = str(uuid.uuid4())
+            if "-c" not in args:
+                args += ["-c", filename]
 
-        assert results['missing config setting'].exit_code != 0
-        assert 'settings are missing from the config file' in results['missing config setting'].output
+            with open(filename, "w") as f:
+                f.write(cases[case]["content"])
 
-        assert results['bad role arn'].exit_code != 0
-        assert 'is not a valid ARN' in results['bad role arn'].output
+            results[case] = runner.invoke(cli.main, args)
+
+        assert results["malformed ini"].exit_code != 0
+        assert "is not a valid INI" in results["malformed ini"].output
+
+        assert results["missing config setting"].exit_code != 0
+        assert "settings are missing from config files" in results["missing config setting"].output
+
+        assert results["bad role arn"].exit_code != 0
+        assert "is not a valid ARN" in results["bad role arn"].output
+
+        assert results["bad output setting"].exit_code != 0
+        assert "`output` in config file" in results["bad output setting"].output
