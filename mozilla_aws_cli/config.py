b682cc6946f81a29c3ab8b5431b5f53e353982a3
April King <april@mozilla.com>
2019-12-10 14:35:34 -0600

    Fix build error wrt Click.

--- mozilla_aws_cli/config.py
+++ mozilla_aws_cli/config.py
@@ -10,14 +10,14 @@ NAME = "Mozilla AWS CLI"
 if IS_WINDOWS:
     DOT_DIR = cache_dir = os.path.join(os.path.expandvars("%APPDATA%"), NAME)
 
-    CONFIG_PATHS = (
+    CONFIG_PATHS = [
         os.path.join(os.path.expandvars("%PROGRAMDATA%"), NAME, "config"),
         os.path.join(DOT_DIR, "config"),
-    )
+    ]
 else:
     DOT_DIR = cache_dir = os.path.join(os.path.expanduser("~"), ".maws")
 
-    CONFIG_PATHS = (
+    CONFIG_PATHS = [
         os.path.join("/etc", "maws", "config"),
         os.path.join(DOT_DIR, "config"),
-    )
+    ]
