70f9210be5d161c0695dca2da323abb8c3223c89
April King <april@mozilla.com>
2019-12-27 16:32:17 -0600

    Switch to using something closer to the XDG specification. Changes config file name to "config.ini".

--- mozilla_aws_cli/config.py
+++ mozilla_aws_cli/config.py
@@ -1,23 +1,42 @@
+import appdirs
 import logging
-import os
+import os.path
+import sys
 
 
 logger = logging.getLogger(__name__)
 
-IS_WINDOWS = os.name == "nt"
+CONFIG_FILE_NAME = "config.ini"
+IS_MACOS = sys.platform == "darwin"
+IS_WINDOWS = sys.platform == "win32"
 NAME = "Mozilla AWS CLI"
+SHORT_NAME = "maws"
 
 if IS_WINDOWS:
-    DOT_DIR = cache_dir = os.path.join(os.path.expandvars("%APPDATA%"), NAME)
+    CACHE_DIR = appdirs.user_cache_dir(NAME, "")
+    CONFIG_DIR = appdirs.user_config_dir(NAME, "", roaming=True)
 
     CONFIG_PATHS = [
-        os.path.join(os.path.expandvars("%PROGRAMDATA%"), NAME, "config"),
-        os.path.join(DOT_DIR, "config"),
+        os.path.join(appdirs.site_config_dir(NAME, ""), CONFIG_FILE_NAME),
+        os.path.join(CONFIG_DIR, CONFIG_FILE_NAME),
+    ]
+elif IS_MACOS:
+    # as this is a CLI app, this forces the app to use the XDG specification
+    # instead of storing its configuration in ~/Library/Application Support
+    appdirs.system = "linux2"
+
+    CACHE_DIR = appdirs.user_cache_dir(SHORT_NAME)
+    CONFIG_DIR = appdirs.user_config_dir(SHORT_NAME)
+
+    CONFIG_PATHS = [
+        os.path.join("/etc", SHORT_NAME, CONFIG_FILE_NAME),  # no XDG in /etc
+        os.path.join(CONFIG_DIR, CONFIG_FILE_NAME),
     ]
 else:
-    DOT_DIR = cache_dir = os.path.join(os.path.expanduser("~"), ".maws")
+    CACHE_DIR = appdirs.user_cache_dir(SHORT_NAME)
+    CONFIG_DIR = appdirs.user_config_dir(SHORT_NAME)
 
     CONFIG_PATHS = [
-        os.path.join("/etc", "maws", "config"),
-        os.path.join(DOT_DIR, "config"),
+        os.path.join(appdirs.site_config_dir(SHORT_NAME), CONFIG_FILE_NAME),
+        os.path.join(CONFIG_DIR, CONFIG_FILE_NAME),
     ]
