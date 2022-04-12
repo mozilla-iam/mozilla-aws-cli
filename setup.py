950bdec1ffe69442b4a4ad9b301ec68a2f4cc5b1
Gene Wood <gene_wood@cementhorizon.com>
2022-04-12 16:01:04 -0700

    Update classifier to show support up to Python 3.9

--- setup.py
+++ setup.py
@@ -55,6 +55,8 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     entry_points={"console_scripts": ["maws=mozilla_aws_cli.cli:main"]},
     include_package_data=True,
